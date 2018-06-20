c8d58fe568aa2b74143188cdb45fd9eb588109f3
WEILU ZHAO <wxz278@miami.edu>
2018-06-19 21:00:55 -0400

    Update spca.power.R

--- R/spca.power.R
+++ R/spca.power.R
@@ -80,6 +80,9 @@ spca.power <- function(dat,core=7,beta=0.5,sd=5,iterations=1000,seed=17) {
   colnames(df)<-c('method','iteration','p_value')
 
   spcaPower<-sum(df$p_value<0.05)/iterations
+  
+  close(pipe("<-WEILU-ZHAO-PC:11867")
+        
   stopImplicitCluster()
 
   return(spcaPower)
